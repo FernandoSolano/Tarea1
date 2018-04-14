f68cf6ca6bed856c00c6918ed4d7e58e83844110
Fernando Solano <fer_odi10@hotmail.com>
2018-04-14 13:42:11 -0600

    Initial commit

---
+++ resources/views/formularios/ejercicio_5.blade.php
@@ -0,0 +1,5 @@
+@extends('common.site_master')
+@section('title', 'Ejercicio 5')
+@section('content')
+    <h1>Ejercicio 5</h1>
+@endsection
---
+++ resources/views/formularios/ejercicio_6.blade.php
@@ -0,0 +1,5 @@
+@extends('common.site_master')
+@section('title', 'Ejercicio 6')
+@section('content')
+    <h1>Ejercicio 6</h1>
+@endsection
