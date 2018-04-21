bb16074f604b03152ba328b6eefe90f3c49a7506
Fernando Solano <fer_odi10@hotmail.com>
2018-04-20 23:27:53 -0600

    update database

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '163.178.173.144'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'tarea1'),
+            'username' => env('DB_USERNAME', 'multi-paraiso'),
+            'password' => env('DB_PASSWORD', 'multimedios.rp.2017'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
